516603f51ac98ac13e14501a06d340045dc1f470
Student <example@example.com>
2019-03-29 10:29:54 -0700

    add secound round of questions to index.html

--- js/scripts.js
+++ js/scripts.js
@@ -5,10 +5,19 @@ $(document).ready(function()  {
       $("#quizLogic").removeClass();
       $("#companySize").addClass("hidden");
       $("form:first-child").addClass("hidden");
+      $("#server").addClass("hidden");
+      $("#mobileApps").addClass("hidden");
     }else{
       $("#quizLogic").removeClass();
       $("#agency").addClass("hidden")
       $("form:first-child").addClass("hidden");
+      $("#server").addClass("hidden");
+      $("#mobileApps").addClass("hidden");
     }
+  });
+  $("#quizLogic").submit(function(event) {
+    event.preventDefault();
+  
+
   });
 });
